dd440afb6f8cae3716b9cbe7a6bba643569ce134
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-01 19:54:06 +0000

    Deploy supakornbabe/YWC17-Web to github.com/supakornbabe/YWC17-Web.git:gh-pages

---
+++ precache-manifest.58dda32c23dbc1f084df33030bd13221.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a354c3dcb78b5b5c9e1d032b867d3f24",
+    "url": "/YWC17-Web/index.html"
+  },
+  {
+    "revision": "4389e9512891a936a54d",
+    "url": "/YWC17-Web/static/css/main.641bab84.chunk.css"
+  },
+  {
+    "revision": "1787233e26cf24b80c36",
+    "url": "/YWC17-Web/static/js/2.c3440187.chunk.js"
+  },
+  {
+    "revision": "4389e9512891a936a54d",
+    "url": "/YWC17-Web/static/js/main.383e94fc.chunk.js"
+  },
+  {
+    "revision": "bfe9140ae493233d9a96",
+    "url": "/YWC17-Web/static/js/runtime-main.0c562df3.js"
+  },
+  {
+    "revision": "6c42699db83442be94bd7a7340c205a6",
+    "url": "/YWC17-Web/static/media/Banner_CGD_Sq.6c42699d.png"
+  },
+  {
+    "revision": "ed683f12a7722cf108f1f5e78c8e38bf",
+    "url": "/YWC17-Web/static/media/Banner_KTB_SQ.ed683f12.png"
+  },
+  {
+    "revision": "1b47ec360e44880a573806ea4c899619",
+    "url": "/YWC17-Web/static/media/Banner_TAT_Hotline_Sq.1b47ec36.png"
+  },
+  {
+    "revision": "093f4f579ff0882a2200c8860e411066",
+    "url": "/YWC17-Web/static/media/CGD.093f4f57.png"
+  },
+  {
+    "revision": "34f69a5bf778b186c5820c69466ecf2d",
+    "url": "/YWC17-Web/static/media/FPO.34f69a5b.png"
+  },
+  {
+    "revision": "8b71f26b142425c9db5075c674c7fe5e",
+    "url": "/YWC17-Web/static/media/Krungthai.8b71f26b.png"
+  },
+  {
+    "revision": "398d843b5815a120bc15d604003b7dde",
+    "url": "/YWC17-Web/static/media/MOF.398d843b.png"
+  },
+  {
+    "revision": "094779696e270e825510a8e0e5de0cd9",
+    "url": "/YWC17-Web/static/media/MOTS.09477969.png"
+  },
+  {
+    "revision": "2529bd05f4a98e5215edc2d91f0597bb",
+    "url": "/YWC17-Web/static/media/TAT.2529bd05.png"
+  },
+  {
+    "revision": "45415b3d8382c9a11e16af5f88efab0e",
+    "url": "/YWC17-Web/static/media/TATSanaChon-Bold.45415b3d.woff2"
+  },
+  {
+    "revision": "29c3fec8f8a81165e4a924a4f50df8d9",
+    "url": "/YWC17-Web/static/media/TATSanaChon-Regular.29c3fec8.woff2"
+  },
+  {
+    "revision": "f51f1b81ee331401e0118c8e97c18083",
+    "url": "/YWC17-Web/static/media/TATSanaSuksa-Regular.f51f1b81.woff2"
+  },
+  {
+    "revision": "966811a960d397493ef173c8f52f9925",
+    "url": "/YWC17-Web/static/media/banner-center-s.966811a9.png"
+  },
+  {
+    "revision": "9e7d7f4a11dd5276a2b04c6c17ac89c2",
+    "url": "/YWC17-Web/static/media/banner-center.9e7d7f4a.png"
+  },
+  {
+    "revision": "c68255bfa3803878d8aad80aa40e1fa3",
+    "url": "/YWC17-Web/static/media/banner-left-s.c68255bf.png"
+  },
+  {
+    "revision": "1b48104080cd8f7e3dd700dc98c70745",
+    "url": "/YWC17-Web/static/media/banner-left.1b481040.png"
+  },
+  {
+    "revision": "ef137c7842892e8e23e2200662e25cbd",
+    "url": "/YWC17-Web/static/media/banner-right-s.ef137c78.png"
+  },
+  {
+    "revision": "b28170f021a607a59c649ae7b8ff8cf5",
+    "url": "/YWC17-Web/static/media/banner-right.b28170f0.png"
+  },
+  {
+    "revision": "0b9c7c9cd693cf575501518e1a493198",
+    "url": "/YWC17-Web/static/media/banner.0b9c7c9c.png"
+  }
+]);
